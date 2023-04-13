8b46ab0264f3b15e460475460d6d8b30f4885c0f
Anar-Erdene01 <126750338+Anar-Erdene01@users.noreply.github.com>
2023-04-13 07:04:55 +0000

    nice

--- README.md
+++
@@ -1,2 +0,0 @@
-# button-change
-nc bro
--- haha.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById("myButton");
-const square = document.querySelector(".square");
-
-button.addEventListener("click", function() {
-  button.classList.add("clicked");
-  square.style.backgroundColor = "olivegreen";
-});
